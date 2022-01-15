5b57cfa7937c7165b70efd463a7203a72b5e755f
Jamshed Ansari <ansarijamshed902@gmail.com>
2022-01-15 19:05:50 +0530

    my first commit reactwebsite

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './Components/Navbar';
+import Contact from './Components/Contact';
+import Footer from './Components/Footer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+   <div className="page-container">
+     <div className="content-wrap">
+
+     
+    <Navbar />
+    <Contact />
     </div>
+    <Footer />
+    </div>
+    </>
   );
 }
 
